9a21b9fcf9d68fce8d98ab71c589abd3ac22f4d6
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-07-23 23:49:37 +0300

    Create model.java

---
+++ model.java
@@ -0,0 +1,13 @@
+public class model extends araba { //subclass2
+
+    public model(int yolcuSayisi,String renk,String marka,double fiyat){
+        super(yolcuSayisi,renk,marka,fiyat);
+    }
+    public void corsa(){
+       super.bilgiGoster();
+    }public void civic(){
+      super.bilgiGoster();
+    }public void doblo(){
+       super.bilgiGoster();
+    }
+}
